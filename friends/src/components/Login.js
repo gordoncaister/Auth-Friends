396407115d1d6aed83ebdf6f25a03be2f59feb0e
Gordon Caister <gordoncaister@gmail.com>
2020-04-21 09:00:15 -0500

    created simple handlesubmit for login

--- friends/src/components/Login.js
+++ friends/src/components/Login.js
@@ -1,14 +1,28 @@
 import React, {useState} from "react"
+import axio from "axios"
 
 import {useInput} from "./useInput"
+import Axios from "axios";
 const Login = () => {
 
     const [username, setUsername, handleUsername] = useInput("Lambda School");
     const [password, setPassword, handlePassword] = useInput("i<3Lambd4");
     
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        Axios.post("/api/login",{ username: username, password: password })
+        .then(res =>{
+            console.log(res)
+        })
+        .catch(err => {
+            console.log(err)
+        })
+    }
+
+
     return(
         <>
-        <form>
+        <form onSubmit={handleSubmit}>
             <label htmlFor="username">Username
             <input
             id="username"
@@ -29,7 +43,7 @@ const Login = () => {
             value={password}
             />
             </label>
-            <button>Submit</button>
+            <button type="submit">Submit</button>
         </form>   
         </>
     )
