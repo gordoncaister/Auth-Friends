ccd7139bd7073f26d5bf8f8df255a38aafb4e6a2
Gordon Caister <gordoncaister@gmail.com>
2020-04-21 08:52:30 -0500

    setup login input handling

--- friends/src/components/Login.js
+++ friends/src/components/Login.js
@@ -1,16 +1,33 @@
-import React from "react"
+import React, {useState} from "react"
+
+import {useInput} from "./useInput"
 const Login = () => {
 
-
-
+    const [username, setUsername, handleUsername] = useInput("Lambda School");
+    const [password, setPassword, handlePassword] = useInput("i<3Lambd4");
+    
     return(
         <>
         <form>
-            <label>Username
-            <input/>
+            <label htmlFor="username">Username
+            <input
+            id="username"
+            name="username"
+            onChange={e => handleUsername(e.target.value)}
+            placeholder="Username"
+            type="text"
+            value={username}
+            />
             </label>
-            <label>Password
-            <input/>
+            <label htmlFor="password">Password
+            <input
+            id="password"
+            name="password"
+            onChange={e => handlePassword(e.target.value)}
+            placeholder="Password"
+            type="password"
+            value={password}
+            />
             </label>
             <button>Submit</button>
         </form>   
